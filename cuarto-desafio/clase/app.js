68eb596e8c4d0bb104251704fca103ee608526aa
ubuntu <sinazay123@gmail.com>
2021-01-25 18:26:59 -0500

    cuarto desafio

---
+++ cuarto-desafio/clase/app.js
@@ -0,0 +1,34 @@
+// function random(min,max) {
+//     return Math.floor(Math.random()* (max -min)) +min
+// }
+
+// let obj = {
+//     0:2,
+//     1:3,
+//     2:6,
+// }
+
+// let keysArray = Object.keys(obj)
+// console.log(keysArray.includes('0'))
+// // console.log(Object.keys(obj))
+// // console.log(Object.values(obj))
+
+// for (let i =0; i < 1000; i++){
+//     let n = random(1, 20)
+//     if(Object.keys(keysArray).includes(n.toString())) 
+//         obj[n]++
+//     else obj[n] = 1
+// }
+
+// console.log(obj)
+
+
+// let productos = [{}]
+// let arrNombres = productos.map( e => e.nombre).toString()
+// productos.forEach(e=> {
+//     arrNombres.push(e.nombre)
+// })
+
+// console.log(arrNombres.toString())
+
+// let total = productos.map( e => e.precio)
