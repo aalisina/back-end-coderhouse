3ad38ef14c90ab2949b1e2c79f79e0e1b946e253
ubuntu <sinazay123@gmail.com>
2021-02-03 22:55:25 -0500

    desafio

--- septimo-desafio/entregable/index.ts
+++ septimo-desafio/entregable/index.ts
@@ -14,15 +14,27 @@ const PORT = process.env.PORT || 8080
 
 let productos: Producto[] = []
 
-const addProduct = (producto: Producto): string => {
-    productos = [...productos, producto]
-    return 'Product added'
-}
+// const addProduct = (producto: Producto): string => {
+//     productos = [...productos, producto]
+//     return 'Product added'
+// }
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hola mundo')
 })
 
+let countItem: number = 0
+let countItems: number = 0
+
+app.get('/items', (req: Request, res: Response) => {
+    const objToSend = {
+        items: productos,
+        cantidad: productos.length
+    }
+    countItems++
+    res.send(objToSend)
+})
 
 app.listen(PORT, ()=> {
     console.log(`Server listening on port ${PORT}`)
