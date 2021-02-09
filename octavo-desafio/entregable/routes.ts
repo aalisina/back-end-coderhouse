7551f822d4963b59bf240f69318a56453a879519
ubuntu <sinazay123@gmail.com>
2021-02-09 11:35:31 -0500

    desafio octavo

--- octavo-desafio/entregable/routes.ts
+++ octavo-desafio/entregable/routes.ts
@@ -15,11 +15,11 @@ router.get('/productos/:id', (req: Request, res: Response) => {
     if(idNum > data.length || idNum <= 0) res.json({error: 'Producto no encontrado'})
     const objToSend = {
         id: idNum,
-        title: data[data[idNum-1].title,
-        price: data[data[idNum-1].price,
-        thumbnail: data[data[idNum-1].thumbnail,
+        title: data[idNum-1].title,
+        price: data[idNum-1].price,
+        thumbnail: data[idNum-1].thumbnail,
     }
-    
+
     res.json(objToSend)
   
 })
