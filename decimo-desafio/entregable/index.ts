64c72ae453a09cc38e051192c8bf7d51ac7a3b1c
ubuntu <sinazay123@gmail.com>
2021-02-16 23:25:51 -0500

    desafio 10

--- decimo-desafio/entregable/index.ts
+++ decimo-desafio/entregable/index.ts
@@ -1,5 +1,7 @@
 import express, {Application, Request, Response} from 'express'
 import routes from './routes'
+import {data} from './database'
+const handlebars =require('express-handlebars') 
 
 const app: Application = express()
 app.use(express.json())
@@ -14,4 +16,26 @@ const PORT = process.env.PORT || 8080
 
 app.listen(PORT, ()=> {
     console.log(`Listening at ${PORT}`)
+})
+
+app.engine("hbs",handlebars({
+
+    extname:".hbs",
+    defaultLayout:"index.hbs",
+    layoutsDir:__dirname+"/views/layouts/",
+    partialsDir:__dirname +"/views/partials/"
+    
+    
+    }))
+app.set('views','./views/layout')
+app.set('view engine','hbs')
+
+app.get('/productos/vista', (req: Request, res: Response) => {
+        if(!data) return res.json({error: 'No hay productos cargados'})
+        const objToSend = data.map( (item, index)=> {
+            return {
+                id: "Hola"
+            }
+        })
+        res.render("index", objToSend)
 })
--- decimo-desafio/entregable/routes.ts
+++ decimo-desafio/entregable/routes.ts
@@ -1,39 +1,38 @@
 import express, {json, Request, Response} from 'express'
 import  fs  from 'fs'
 import { data } from './database'
+const handlebars = require("express-handlebars")
 
 const router = express.Router()
 
+// router.get('/productos', (req: Request, res: Response) => {
+//     if(!data) return res.json({error: 'No hay productos cargados'})
+//     const objToSend = data.map( (item, index)=> {
+//         return {
+//             id: index+1,
+//             title: item.title,
+//             price: item.price,
+//             thumbnail: item.thumbnail
+//         }
+//     })
+//     res.json(objToSend)
+// })
 
+// router.get('/productos/:id', (req: Request, res: Response) => {
+//     const id  = req.params.id
+//     const idNum = Number(id)
+//     if(idNum === NaN) return res.json({error: 'Producto no encontrado'})
+//     if(idNum > data.length || idNum <= 0) return res.json({error: 'Producto no encontrado'})
+//     const objToSend = {
+//         id: idNum,
+//         title: data[idNum-1].title,
+//         price: data[idNum-1].price,
+//         thumbnail: data[idNum-1].thumbnail
+//     }
 
-router.get('/productos', (req: Request, res: Response) => {
-    if(!data) return res.json({error: 'No hay productos cargados'})
-    const objToSend = data.map( (item, index)=> {
-        return {
-            id: index+1,
-            title: item.title,
-            price: item.price,
-            thumbnail: item.thumbnail
-        }
-    })
-    res.json(objToSend)
-})
-
-router.get('/productos/:id', (req: Request, res: Response) => {
-    const id  = req.params.id
-    const idNum = Number(id)
-    if(idNum === NaN) return res.json({error: 'Producto no encontrado'})
-    if(idNum > data.length || idNum <= 0) return res.json({error: 'Producto no encontrado'})
-    const objToSend = {
-        id: idNum,
-        title: data[idNum-1].title,
-        price: data[idNum-1].price,
-        thumbnail: data[idNum-1].thumbnail
-    }
-
-    res.json(objToSend)
+//     res.json(objToSend)
   
-})
+// })
 
 router.post('/productos/', (req: Request, res: Response) => {
     const objToCreate = req.body
